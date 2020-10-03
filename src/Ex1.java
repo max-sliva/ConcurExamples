6eb300870715b2e77e3e19cfafadc0eacafa87f7
max-sliva <persimon@inbox.ru>
2020-10-03 15:35:37 +0500

    Ex2 added

--- src/Ex1.java
+++ src/Ex1.java
@@ -1,15 +1,44 @@
+import java.util.Date;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Ex1 {
 
 	public static void main(String[] args) {
-		ExecutorService executor = Executors.newSingleThreadExecutor();
-		executor.submit(() -> {
-		    String threadName = Thread.currentThread().getName();
-		    System.out.println("Hello " + threadName);
+		Date currentDate=new Date();
+		long time1=currentDate.getTime();
+		ExecutorService executor = Executors.newFixedThreadPool(2);
+		executor.execute(() -> {
+			String threadName = Thread.currentThread().getName();
+			while (Thread.currentThread().getState() == Thread.State.RUNNABLE) {
+				System.out.println("Hello " + threadName);
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+//					e.printStackTrace();
+					return;
+				}
+			}
 		});
-
+		executor.execute(() -> {
+			String threadName = Thread.currentThread().getName();
+			while (Thread.currentThread().getState() == Thread.State.RUNNABLE) {
+				System.out.println("Hello " + threadName);
+				try {
+					Thread.sleep(500);
+				} catch (InterruptedException e) {
+//					e.printStackTrace();
+					return;
+				}
+			}
+		});
+		currentDate=new Date();
+		long time2=currentDate.getTime();;
+		while ((time2-time1)<2000) { //цикл, пока разница не составит 2 секунды
+			currentDate=new Date();
+			time2=currentDate.getTime();
+		}
+		executor.shutdownNow();
 	}
 
 }
---
+++ src/Ex2.java
@@ -0,0 +1,23 @@
+import java.util.Date;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Ex2 {
+	public static void main(String[] args) {
+		String[] threads = {"A", "B"}; 
+		Date currentDate=new Date();
+		long time1=currentDate.getTime();
+		ExecutorService executor = Executors.newFixedThreadPool(2);
+		for (String string : threads) {
+			executor.execute(new SimpleRunnable(string));
+		} 
+		currentDate=new Date();
+		long time2=currentDate.getTime();;
+		while ((time2-time1)<2000) { //цикл, пока разница не составит 2 секунды
+			currentDate=new Date();
+			time2=currentDate.getTime();
+		}
+		executor.shutdownNow();
+		System.out.println("2 seconds");
+	}
+}
