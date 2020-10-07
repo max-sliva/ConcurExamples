00f851b7e2682730b40c907f3a27f5be8ca5ae0c
max-sliva <persimon@inbox.ru>
2020-10-07 15:44:05 +0500

    changes in Ex4

--- src/Ex4.java
+++ src/Ex4.java
@@ -1,19 +1,44 @@
-import java.util.Date;
-import java.util.concurrent.ExecutorService;
+import java.awt.BorderLayout;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
-public class Ex4 {
-	public static void main(String[] args) {
-		String[] threads = {"A", "B"};
-		int[] times = {4000, 5000};
-		ScheduledExecutorService executor = Executors.newScheduledThreadPool(2);
-		int i = 0;
-		for (String string : threads) {
-			executor.scheduleAtFixedRate(new SimpleRunnable2(string, times[i++]),1,2,TimeUnit.SECONDS);
-		} 
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
 
+public class Ex4 {
+	static ScheduledExecutorService executor = null;
+	public static void main(String[] args) {
+		JFrame myFrame = new JFrame("ScheduledExecutorService example");
+		myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Box northBox = new Box(BoxLayout.X_AXIS);
+		JButton startThreads = new JButton("Start threads!");
+		northBox.add(startThreads);
+		northBox.add(Box.createHorizontalGlue());
+		JButton stopThreads = new JButton("Stop threads!");
+		northBox.add(stopThreads);
+		
+		myFrame.add(northBox, BorderLayout.NORTH);
+		myFrame.pack();
+		myFrame.setMinimumSize(myFrame.getSize());
+		myFrame.setLocationRelativeTo(null);
+		myFrame.setVisible(true);
+		String[] threads = {"A", "B"};
+		int[] times = {1000, 1000};
+		
+		startThreads.addActionListener(arg0->{
+			int i = 0;
+			executor = Executors.newScheduledThreadPool(2);
+			for (String string : threads) {
+				executor.scheduleAtFixedRate(new SimpleRunnable2(string, times[i++]),1,2,TimeUnit.SECONDS);
+			} 
+		});
+
+		stopThreads.addActionListener(l->{
+			executor.shutdownNow();
+		});	
 		System.out.println("End main");
 	}
 }
